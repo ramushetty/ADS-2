422a81b118f78c829c2a2429eae7a7c3c83260ee
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-01 10:09:30 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2- Assignments/m1/1.1 Graph/1.1 Graph/Solution.java
+++ ADS2- Assignments/m1/1.1 Graph/1.1 Graph/Solution.java
@@ -9,37 +9,37 @@ interface Graph {
 	 *
 	 * @return     { description_of_the_return_value }
 	 */
-    public int V();
-    /**
-     * { function_description }
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public int E();
-    /**
-     * Adds an edge.
-     *
-     * @param      v     { parameter_description }
-     * @param      w     { parameter_description }
-     */
-    public void addEdge(int v, int w);
-    /**
-     * { function_description }
-     *
-     * @param      v     { parameter_description }
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public Iterable<Integer> adj(int v);
-    /**
-     * Determines if it has edge.
-     *
-     * @param      v     { parameter_description }
-     * @param      w     { parameter_description }
-     *
-     * @return     True if has edge, False otherwise.
-     */
-    public boolean hasEdge(int v, int w);
+	public int V();
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int E();
+	/**
+	 * Adds an edge.
+	 *
+	 * @param      v     { parameter_description }
+	 * @param      w     { parameter_description }
+	 */
+	public void addEdge(int v, int w);
+	/**
+	 * { function_description }
+	 *
+	 * @param      v     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public Iterable<Integer> adj(int v);
+	/**
+	 * Determines if it has edge.
+	 *
+	 * @param      v     { parameter_description }
+	 * @param      w     { parameter_description }
+	 *
+	 * @return     True if has edge, False otherwise.
+	 */
+	public boolean hasEdge(int v, int w);
 }
 /**
  * Class for graphh.
@@ -109,25 +109,25 @@ class Graphh implements Graph {
 	 *
 	 * @return     { description_of_the_return_value }
 	 */
-    public Iterable<Integer> adj(int v) {
-    	return adj[v];
-    }
-    /**
-     * Determines if it has edge.
-     *
-     * @param      v     { parameter_description }
-     * @param      w     { parameter_description }
-     *
-     * @return     True if has edge, False otherwise.
-     */
-    public boolean hasEdge(int v, int w) {
-    	for (int k : adj[v]) {
-    		if (k == w) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+	/**
+	 * Determines if it has edge.
+	 *
+	 * @param      v     { parameter_description }
+	 * @param      w     { parameter_description }
+	 *
+	 * @return     True if has edge, False otherwise.
+	 */
+	public boolean hasEdge(int v, int w) {
+		for (int k : adj[v]) {
+			if (k == w) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 
 }
@@ -166,32 +166,34 @@ public final class Solution {
 		if (vertices <= 1 && edges <= 1) {
 			System.out.println("No edges");
 			return;
-		} 
-		switch(names) {
-			case "List":
-				StringBuilder s = new StringBuilder();
-				for (int i = 0; i < vertices; i++) {
-					s.append(map.get(i) + ": ");
-					for (int k : g.adj[i]) {
-						s.append(map.get(k) + " ");
-					}
-					s.append("\n");
+		}
+		switch (names) {
+		case "List":
+			StringBuilder s = new StringBuilder();
+			for (int i = 0; i < vertices; i++) {
+				s.append(map.get(i) + ": ");
+				for (int k : g.adj[i]) {
+					s.append(map.get(k) + " ");
 				}
-				System.out.println(s.toString());
-				break;
-			case "Matrix":
-				for (int i = 0; i < vertices; i++) {
-					for (int j = 0; j < vertices; j++) {
-						if (g.hasEdge(i, j)) {
-							System.out.print("1 ");
-						} else {
-							System.out.print("0 ");
-						}
+				s.append("\n");
+			}
+			System.out.println(s.toString());
+			break;
+		case "Matrix":
+			for (int i = 0; i < vertices; i++) {
+				for (int j = 0; j < vertices; j++) {
+					if (g.hasEdge(i, j)) {
+						System.out.print("1 ");
+					} else {
+						System.out.print("0 ");
 					}
-					System.out.println();
-					
 				}
-				break;
+				System.out.println();
+
+			}
+			break;
+		default:
+			break;
 		}
 	}
 }
