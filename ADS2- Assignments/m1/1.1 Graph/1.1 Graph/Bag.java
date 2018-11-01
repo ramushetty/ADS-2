029154cbb99103e265054725147e3ac3cfd862b1
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-01 10:56:56 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2- Assignments/m1/1.1 Graph/1.1 Graph/Bag.java
+++ ADS2- Assignments/m1/1.1 Graph/1.1 Graph/Bag.java
@@ -10,11 +10,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
+ *  The <tt>Bag</tt> class represents a bag (or multiset) of
+ *  generic items. It supports insertion and iterating over the
  *  items in arbitrary order.
  *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation
  *  take constant time. Iteration takes time proportional to the number of items.
  *  <p>
  *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
@@ -30,31 +30,31 @@ public class Bag<Item> implements Iterable<Item> {
         private Node next;
     }
 
-   /**
-     * Create an empty stack.
-     */
+    /**
+      * Create an empty stack.
+      */
     public Bag() {
         first = null;
         N = 0;
     }
 
-   /**
-     * Is the BAG empty?
-     */
+    /**
+      * Is the BAG empty?
+      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the bag.
-     */
+    /**
+      * Return the number of items in the bag.
+      */
     public int size() {
         return N;
     }
 
-   /**
-     * Add the item to the bag.
-     */
+    /**
+      * Add the item to the bag.
+      */
     public void add(Item item) {
         Node oldfirst = first;
         first = new Node();
@@ -64,11 +64,11 @@ public class Bag<Item> implements Iterable<Item> {
     }
 
 
-   /**
-     * Return an iterator that iterates over the items in the bag.
-     */
+    /**
+      * Return an iterator that iterates over the items in the bag.
+      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator();
     }
 
     // an iterator, doesn't implement remove() since it's optional
@@ -81,7 +81,7 @@ public class Bag<Item> implements Iterable<Item> {
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
