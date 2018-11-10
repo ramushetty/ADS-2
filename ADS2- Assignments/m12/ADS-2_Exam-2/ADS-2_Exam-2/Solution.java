6d2b522702b94ac4ac73b5c9d806210387d43cc1
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-10 12:37:49 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2- Assignments/m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
+++ ADS2- Assignments/m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
@@ -62,7 +62,8 @@ public final class Solution {
 
         case "ViaPaths":
             // Handle the case of ViaPaths, where three integers are given.
-            // First is the source and second is the via is the one where path should pass throuh.
+            // First is the source and second is the via
+            //  is the one where path should pass throuh.
             // third is the destination.
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
