5a79e77b9259abf5a1f7633f7281e79c1c23b35f
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-13 07:12:08 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2- Assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/WordNet.java
+++ ADS2- Assignments/m4/Code Camp - WordNet/Code Camp - WordNet/WordNet/WordNet.java
@@ -12,7 +12,8 @@
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB)
 
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // a synset (second field of synsets.txt)
+     that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB)
 
