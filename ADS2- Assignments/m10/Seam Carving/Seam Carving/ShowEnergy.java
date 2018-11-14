b5d7cceaca5ab1b44d1a5aced0c95dec91fe690c
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-14 11:52:37 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2- Assignments/m10/Seam Carving/Seam Carving/ShowEnergy.java
+++ ADS2- Assignments/m10/Seam Carving/Seam Carving/ShowEnergy.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac ShowEnergy.java
  *  Execution:    java ShowEnergy input.png
  *  Dependencies: SeamCarver.java SCUtility.java
- *                
+ *
  *
  *  Read image from file specified as command line argument. Show original
  *  image (only useful if image is large enough).
@@ -12,15 +12,30 @@
 // import edu.princeton.cs.algs4.Picture;
 // import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Class for show energy.
+ */
 public class ShowEnergy {
-
-    public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private ShowEnergy() {
+		//empty.
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+    public static void main(final String[] args) {
         Picture picture = new Picture(args[0]);
-        StdOut.printf("image is %d columns by %d rows\n", picture.width(), picture.height());
+        StdOut.printf(
+"image is %d columns by %d rows\n", picture.width(), picture.height());
         picture.show();        
         SeamCarver sc = new SeamCarver(picture);
         
-        StdOut.printf("Displaying energy calculated for each pixel.\n");
+        StdOut.printf(
+"Displaying energy calculated for each pixel.\n");
         SCUtility.showEnergy(sc);
 
     }
