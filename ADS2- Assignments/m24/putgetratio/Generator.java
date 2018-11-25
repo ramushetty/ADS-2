017c6d04f22154d2ae7fe4f63be0606ce3e089ab
ramushetty <ummadishettyramu@msitprogram.net>
2018-11-26 03:12:51 +0530

    PutGetRatio

---
+++ ADS2- Assignments/m24/putgetratio/Generator.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Generator {
+	public static Random generator = new Random();
+	
+	/**
+	 * Creates an array filled with random values.
+	 * @param length the length of the array
+	 * @param n the number of possible random values
+	 * @return an array filled with length numbers between 0 and n-1
+	 */
+	public static int[] randomIntArray(int length, int n){
+		int[] a = new int[length];
+		
+		for(int i=0; i<a.length; i++)
+			a[i] = generator.nextInt(n);
+		
+		return a;
+	}
+}
